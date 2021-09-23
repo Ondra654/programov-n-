1a62fe8c33425e582e42b92b7ef1195e2ca42f87
Ondra654 <90826574+Ondra654@users.noreply.github.com>
2021-09-23 09:40:16 +0200

    Add files via upload

---
+++ ConsoleApp3/Program2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            int main()
+        {
+
+            int num1, num2;
+            float result;
+            int oper;
+
+            Console.WriteLine("Enter +,-,* or /");
+            Console.ReadLine("int", oper);
+            Console.WriteLine("Enter first number");
+            Console.ReadLine("int", oper);
+            Console.WriteLine("Enter second number");
+            Console.ReadLine("int", oper);
+
+            result = 0;
+            switch (oper)
+
+
+                case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Invalid operaton");
+
+            }
+
+            Console.WriteLine("Result:")
+
+
+
+        {
+        }
+    }
+}
