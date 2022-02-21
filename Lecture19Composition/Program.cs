0d06a7ab6d046ad17c1ffc9f8a7a0e43fda359b9
Ondra654 <90826574+Ondra654@users.noreply.github.com>
2022-02-21 21:41:00 +0100

    Add files via upload

---
+++ Lecture19Composition/Program.cs
@@ -0,0 +1,38 @@
+﻿ using System;
+
+
+namespace Lecture19Composition
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random randomName = new Random();
+
+			string[] firstnpc = {"Tyler", "Mike", "Winton", "Brian", "Rowan", "Russell", "Derek"};
+			string[] secondnpc = {"Harriet", "Lillian", "Sybil", "Andrea", "Chloe"};
+			string[] thirdnpc = {"Edward", "Freddie", "Trey", "Thomas", "Alfie", "Logan"};
+
+			string FirstName = firstnpc[randomName.Next(firstnpc.Length)];
+			string SecondName = secondnpc[randomName.Next(secondnpc.Length)];
+			string ThirdName = thirdnpc[randomName.Next(thirdnpc.Length)];
+
+			Random random = new Random();
+
+			Character one = new Character(new AI(random), FirstName, 15, 6, 4);
+			Character two = new Character(new AI(random), SecondName, 10, 7, 5);
+			Character three = new Character(new Player(Console.In, Console.Out), ThirdName, 20, 4, 4);
+
+			Game game = new Game(one, two, new Die(random, 6));
+			game.Run(Console.Out);
+			Console.WriteLine();
+
+			Game game2 = new Game(one, three, new Die(random, 6));
+			game2.Run(Console.Out);
+			Console.WriteLine();
+
+			Console.WriteLine("Press any key to quit...");
+			Console.ReadKey();
+		}
+	}
+}
